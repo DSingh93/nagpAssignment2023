4433c2c365ba4be07e66bbd3b8ef72ef59cd1c7d
Dhananjay Singh <dhananjay.singh@nagarro.com>
2023-08-13 22:41:49 +0530

    DevTestOps maven project

---
+++ src/test/java/devopsDemo/LaunchBrowser.java
@@ -0,0 +1,21 @@
+package devopsDemo;
+
+import org.testng.annotations.Test;
+
+public class LaunchBrowser {
+
+	@Test
+	public void myTest() {
+		System.out.println("I am in Test 1");
+	}
+	
+	@Test
+	public void myTest2() {
+		System.out.println("I am in Test 2");
+	}
+	
+	@Test
+	public void myTest3() {
+		System.out.println("I am in Test 3");
+	}
+}
